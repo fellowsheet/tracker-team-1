648c6b68890ff6bb428df6d65499623294ac6cee
Simon <vonahkahs.s@gmail.com>
2024-06-14 09:22:15 +0400

    create .env file and connecting with main file

---
+++ telegram/config_data/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+if not find_dotenv():
+    exit("Environment variables are not loaded because the file is missing .env")
+else:
+    load_dotenv()
+    
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
--- telegram/main.py
+++ telegram/main.py
@@ -2,12 +2,17 @@ import logging
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from config_data.config import BOT_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def main():
-    bot = Bot(token='')
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode='HTML'),)
     dp = Dispatcher()
 
     await bot.delete_webhook(drop_pending_updates=True)
